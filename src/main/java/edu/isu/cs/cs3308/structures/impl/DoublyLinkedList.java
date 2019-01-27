737b3de580abfde312c74164a0efb1a887a5088d
Kirill Brainard <braikiri@isu.edu>
2019-01-27 11:58:55 -0700

    Initial commit, added singly linked list class from previous assignment, and added all required methods from the interfaces

---
+++ src/main/java/edu/isu/cs/cs3308/structures/impl/DoublyLinkedList.java
@@ -0,0 +1,55 @@
+package edu.isu.cs.cs3308.structures.impl;
+
+import edu.isu.cs.cs3308.structures.List;
+
+public class DoublyLinkedList<E> implements List<E> {
+
+    @Override
+    public E first(){
+       return null;
+    }
+
+    public E last(){
+        return null;
+    }
+
+    public void addLast(E element){
+
+    }
+
+    public void addFirst(E element){
+
+    }
+
+    public E removeFirst(){
+        return null;
+    }
+
+    public E removeLast(){
+        return null;
+    }
+
+    public void insert(E element, int index){
+
+    }
+
+    public E remove(int index){
+        return null;
+    }
+
+    public E get(int index){
+        return null;
+    }
+
+    public int size(){
+        return 0;
+    }
+
+    public boolean isEmpty(){
+        return false;
+    }
+
+    public void printList(){
+
+    }
+}
---
+++ src/main/java/edu/isu/cs/cs3308/structures/impl/LinkedStack.java
@@ -0,0 +1,43 @@
+package edu.isu.cs.cs3308.structures.impl;
+
+import edu.isu.cs.cs3308.structures.Stack;
+
+public class LinkedStack<E> implements Stack<E> {
+
+
+    public void push(E element){
+
+    }
+
+    public E peek(){
+        return null;
+    }
+
+    public E pop(){
+        return null;
+    }
+
+    public int size(){
+        return 0;
+    }
+
+    public boolean isEmpty(){
+        return false;
+    }
+
+    public void transfer(Stack<E> to){
+
+    }
+
+    public void reverse(){
+
+    }
+
+    public void merge(Stack<E> other){
+
+    }
+
+    public void printStack(){
+
+    }
+}
