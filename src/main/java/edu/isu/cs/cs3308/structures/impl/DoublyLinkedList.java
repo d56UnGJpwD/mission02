5637827e096e6f64b698efffeaa25658eae396d0
Kirill Brainard <braikiri@isu.edu>
2019-01-27 16:43:33 -0700

    Doubly linked list completed

--- src/main/java/edu/isu/cs/cs3308/structures/impl/DoublyLinkedList.java
+++ src/main/java/edu/isu/cs/cs3308/structures/impl/DoublyLinkedList.java
@@ -1,55 +1,154 @@
 package edu.isu.cs.cs3308.structures.impl;
 
+import com.sun.xml.internal.ws.api.message.Header;
 import edu.isu.cs.cs3308.structures.List;
 
 public class DoublyLinkedList<E> implements List<E> {
 
+    public class Node<E> {
+        private E element;
+        private Node<E> next;
+        private Node<E> prev;
+
+        public void setNext(Node<E> next){
+            this.next = next;
+        }
+
+        public void setPrev(Node<E> prev){ this.prev = prev;}
+
+        public Node<E> getNext(){
+            return next;
+        }
+
+        public Node<E> getPrev(){ return prev; }
+
+        public E getElement(){
+            return element;
+        }
+
+        public Node(E e, Node<E> previous, Node<E> next){
+            this.element = e;
+            this.prev = previous;
+            this.next = next;
+        }
+    }
+
+    private Node<E> header;
+    private Node<E> trailer;
+    private int size = 0;
+
+    public DoublyLinkedList(){
+        header = new Node<>(null, null, null);
+        trailer = new Node<>(null,header,null);
+        header.setNext(trailer);
+    }
+
     @Override
     public E first(){
-       return null;
+        if(isEmpty()){
+            return null;
+        }
+        return header.getNext().getElement();
     }
 
     public E last(){
-        return null;
-    }
-
-    public void addLast(E element){
-
+        if(isEmpty()){
+            return null;
+        }
+        return trailer.getPrev().getElement();
     }
 
     public void addFirst(E element){
+        addBetween(element, header, header.getNext());
+    }
 
+    public void addLast(E element){
+        addBetween(element, trailer.getPrev(), trailer);
     }
 
     public E removeFirst(){
-        return null;
+        if(isEmpty()){
+            return null;
+        }
+        return remove(0);
     }
 
     public E removeLast(){
-        return null;
+        if(isEmpty()){
+            return null;
+        }
+        return remove(size -1);
+    }
+
+    private void addBetween(E e, Node<E> predecessor, Node<E> successor){
+        if(e != null){
+            Node<E> newest = new Node<>(e, predecessor, successor);
+            predecessor.setNext(newest);
+            successor.setPrev(newest);
+            size++;
+        }
     }
 
     public void insert(E element, int index){
-
+        if(index >= 0 && index < size && element != null){
+            addBetween(element, getNode(index).prev, getNode(index));
+        }
+        else{
+        }
     }
 
     public E remove(int index){
+        if(index >= 0 && index < size){
+            Node<E> removed = getNode(index);
+            Node<E> predecessor = removed.getPrev();
+            Node<E> successor = removed.getNext();
+
+            predecessor.setNext(successor);
+            successor.setPrev(predecessor);
+            size--;
+            return removed.getElement();
+        }
         return null;
     }
 
     public E get(int index){
+        if(index >= 0 && index < size) {
+            return getNode(index).getElement();
+        }
+        else{
+            return null;
+        }
+    }
+
+    private Node<E> getNode(int index){
+        if(index >= 0 && index < size){
+            Node<E> current = header;
+            int i = 0;
+
+            while(current != null){
+                if(i == index){
+                    return current.next;
+                }
+                i++;
+                current = current.next;
+            }
+        }
         return null;
     }
 
     public int size(){
-        return 0;
+        return size;
     }
 
     public boolean isEmpty(){
-        return false;
+        return size == 0;
     }
 
     public void printList(){
-
+        Node<E> current = header.next;
+        while(current.next != null){
+            System.out.println(current.getElement());
+            current = current.next;
+        }
     }
 }
--- src/main/java/edu/isu/cs/cs3308/structures/impl/LinkedStack.java
+++ src/main/java/edu/isu/cs/cs3308/structures/impl/LinkedStack.java
@@ -5,9 +5,42 @@ import edu.isu.cs.cs3308.structures.Stack;
 public class LinkedStack<E> implements Stack<E> {
 
 
-    public void push(E element){
+    public class Node<E> {
+        private E element;
+        private Node<E> next;
 
+        public void setNext(Node<E> next){
+            this.next = next;
+        }
+
+        public Node<E> getNext(){
+            return next;
+        }
+
+        public E getElement(){
+            return element;
+        }
+
+        public Node(E e, Node<E> next){
+            this.element = e;
+            this.next = next;
+        }
     }
+
+
+    private int size;
+    private Node<E> first;
+
+    public LinkedStack(){
+        first = null;
+        size = 0;
+    }
+
+    public void push(E element){
+        Node<E> old = first;
+        first = new Node(element, old);
+    }
+
 
     public E peek(){
         return null;
@@ -22,7 +55,10 @@ public class LinkedStack<E> implements Stack<E> {
     }
 
     public boolean isEmpty(){
-        return false;
+        if(first == null) {
+            return true;
+        }
+        else{return false;}
     }
 
     public void transfer(Stack<E> to){
